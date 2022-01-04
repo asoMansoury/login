32af415fa09dd696c7a7ba84f6e11c1a00859999
hastijbr <hastihj383@gmail.com>
2022-01-04 15:47:32 +0330

    loginbasedesign

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/utils/theme';
+import React from "react"
+import PropTypes from "prop-types"
+import Head from "next/head"
+import { ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import theme from "../src/utils/theme"
 import "../src/assests/styles/global.scss"
 import "../styles/loginpage/loginpage.scss"
 
-
 export default function MyApp(props) {
-  const { Component, pageProps } = props;
+  const { Component, pageProps } = props
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
 
   return (
     <React.Fragment>
       <Head>
         <title>Login</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
-         <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-     
     </React.Fragment>
-  );
+  )
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
-};
+}
